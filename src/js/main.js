c6743fa401c88a58a8355b6a36fb55dd65b48946
Vitor Delfino <91512605+vtr-delfino@users.noreply.github.com>
2023-10-01 11:40:35 -0300

    Add Task func (no save)

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ const Main = {
 
     cacheSelector: function() {
         this.$checkButtons = document.querySelectorAll('.check')
+        this.$inputTask = document.querySelector('#inputTask')
+        this.$list = document.querySelector('#list')
     },
 
     bindEvents: function() {
@@ -13,7 +15,9 @@ const Main = {
         
         this.$checkButtons.forEach(button => {
             button.onclick = self.Events.checkButton_click
-        });
+        })
+
+        this.$inputTask.onkeypress = self.Events.inputTask_keyPress.bind(this)
     },
 
     Events: {
@@ -26,6 +30,23 @@ const Main = {
                 return $li.classList.add('done')
             } 
             $li.classList.remove('done')
+        },
+
+        inputTask_keyPress: function(e) {
+            const key = e.key
+            const value = e.target.value
+
+            if(key === 'Enter'){
+                this.$list.innerHTML += `
+                    <li>
+                        <div class="check"></div>
+                        <label class="task">${value}</label>
+                        <button class="remove"></button>
+                    </li>
+                `
+
+                e.target.value = ''
+            }
         }
     }
 }
